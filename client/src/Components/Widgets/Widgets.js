3ca36a1de9be60563fee6b09a63dcbef4e8e5899
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-09-21 10:18:13 +0200

    added weather updater and animation for update button

--- client/src/Components/Widgets/Widgets.js
+++ client/src/Components/Widgets/Widgets.js
@@ -9,26 +9,44 @@ import GetLocale from '../../Services/GetLocale';
 export default function Widgets() {
   const weather = useRef();
   const [weatherImg, setWeatherImg] = useState(Sunny);
+  const [updateWeather, setUpdateWeather] = useState(true);
+  const [useEffectUpdater, setUseEffectUpdater] = useState(false);
 
   let localeTime = GetLocale.getLocaleTime();
 
+  function weatherUpdater() {
+    setUpdateWeather(true);
+
+    if(!useEffectUpdater) {
+      setUseEffectUpdater(true)
+    }else {
+      setUseEffectUpdater(false)
+    }
+  }
+
   useEffect(() => {
-    async function getWeather() {
-      weather.current = await dynamics.changeWeatherImg(setWeatherImg);
-      console.log("WC = ", weather.current)
+
+    function getWeather() {
+
+      setTimeout(async () => {
+        weather.current = await dynamics.changeWeatherImg(setWeatherImg);
+        console.log("WC = ", weather.current)
+        setUpdateWeather(false);
+      }, 10000);
+      
     }
     getWeather();
-  }, [])
+  }, [useEffectUpdater])
 
   return (
     <div className='widgets-container'>
       <aside className='weather-widget'>
         <img className='weather-icon' alt='icon showing current weather' src={weatherImg} />
-        <p className='weather-temp'>{weather.current ? weather.current.temp.toString().slice(0, 2) : "Error"}{'°'}</p>
+        <p className='weather-temp'>{weather.current ? weather.current.temp.toString().slice(0, 2) : "20"}{'°'}</p>
         <div className='para-container'>
-          <p className='weather-location'>{weather.current ? weather.current.location : "Error"}</p>
+          <p className='weather-location'>{weather.current ? weather.current.location : ""}</p>
           <p className='locale-time'>{localeTime.hour}:{localeTime.minute}</p>
-          <img className='refresh-arrow' alt="refresh weather button" src={RefreshArrow}/>
+          <img className={updateWeather ? 'refresh-arrow-active' : 'refresh-arrow'} alt="refresh weather button" src={RefreshArrow} onClick={weatherUpdater}/>
         </div>
       </aside>
     </div>
