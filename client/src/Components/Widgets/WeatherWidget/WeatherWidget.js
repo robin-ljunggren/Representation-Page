6e966f1d641b64bbaea4f1d4c7cde8320a96fbbe
RobbanBroberg <lafarchetamustatche@outlook.com>
2024-03-11 23:43:53 +0100

    fixed weather widget styling and changed some things in the DOM

--- client/src/Components/Widgets/WeatherWidget/WeatherWidget.js
+++ client/src/Components/Widgets/WeatherWidget/WeatherWidget.js
@@ -3,7 +3,6 @@ import dynamics from '../../../Dynamics/dynamics';
 import Sunny from '../../../img/weather-sunny.png';
 import RefreshArrow from '../../../img/refresh-arrow.png';
 import './WeatherWidget.css';
-import GetLocale from '../../../Services/GetLocale';
 
 
 export default function WeatherWidget() {
@@ -11,8 +10,6 @@ export default function WeatherWidget() {
   const [weatherImg, setWeatherImg] = useState(Sunny);
   const [updateWeather, setUpdateWeather] = useState(true);
   const [useEffectUpdater, setUseEffectUpdater] = useState(false);
-
-  let localeTime = GetLocale.getLocaleTime();
 
   function weatherUpdater() {
     setUpdateWeather(true);
@@ -30,23 +27,28 @@ export default function WeatherWidget() {
 
       setTimeout(async () => {
         weather.current = await dynamics.changeWeatherImg(setWeatherImg);
-        console.log("WC = ", weather.current)
         setUpdateWeather(false);
       }, 10000);
       
     }
     getWeather();
-  }, [useEffectUpdater])
+  }, [useEffectUpdater]);
+
+  // weather.current.location = "lång textsträng hmm"
 
   return (
     <div className='widgets-container'>
       <aside className='weather-widget'>
-        <img className='weather-icon' alt='icon showing current weather' src={weatherImg} />
-        <p className='weather-temp'>{weather.current ? weather.current.temp.toString().slice(0, 2) : "20"}{'°'}</p>
+        <div className='icon-temp-container'>
+          <img className='weather-icon' alt='icon showing current weather' src={weatherImg} />
+          <p className='weather-temp'>{weather.current ? weather.current.temp.toString().slice(0, 2) : "20"}{'°'}</p>
+        </div>
         <div className='para-container'>
-          <p className='weather-location'>{weather.current ? weather.current.location : ""}</p>
-          <p className='locale-time'>{localeTime.hour}:{localeTime.minute}</p>
-          <img className={updateWeather ? 'refresh-arrow-active' : 'refresh-arrow'} alt="refresh weather button" src={RefreshArrow} onClick={weatherUpdater}/>
+          <p className='locale-time'>{weather.current ? weather.current.weather : "Sunny"}</p>
+          <div className='location-arrow-container'>
+            <p className='weather-location'>{weather.current ? weather.current.location : ""}</p>
+            <img className={updateWeather ? 'refresh-arrow-active' : 'refresh-arrow'} alt="refresh weather button" src={RefreshArrow} onClick={weatherUpdater}/>
+          </div>
         </div>
       </aside>
     </div>
